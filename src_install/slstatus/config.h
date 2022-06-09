56d00ae00299c0b97c86498f7144da44c653d457
odn <>
2022-06-09 23:11:54 +0000

    modified:   src_install/slstatus/config.h

--- src_install/slstatus/config.h
+++ src_install/slstatus/config.h
@@ -66,5 +66,5 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ datetime, "%%{c} [ %s ] - ",           "%F %T" },
-	{ battery_perc, " [ Battery %s%% ]",           "BAT0" },
+	{ battery_perc, "[ Battery %s%% ]",           "BAT0" },
 };
